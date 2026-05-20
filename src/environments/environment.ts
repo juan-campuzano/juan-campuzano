fc457ff2602a09284fd5ee423c7d0927f2604044
Juan Diego Campuzano Osorio <juan-campuzano@Juans-MacBook-Air.local>
2026-05-20 17:06:13 -0500

    add page

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDZRDTM1NbaZC9uq_fRH0U6F14xYVbGrXc",
+    authDomain: "juan-campuzano.firebaseapp.com",
+    projectId: "juan-campuzano",
+    storageBucket: "juan-campuzano.firebasestorage.app",
+    messagingSenderId: "464309615466",
+    appId: "1:464309615466:web:d269a2790e0040b2808313"
+  }
+};
